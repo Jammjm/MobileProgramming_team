20ad272c54ceb145656456046a2e1bcd9910d57d
Jammjm <jammjmdev@gmail.com>
2021-12-17 00:43:26 +0900

    12

---
+++ app/src/main/java/com/example/recyclingwiki/metalcan.kt
@@ -0,0 +1,11 @@
+package com.example.recyclingwiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class metalcan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metalcan)
+    }
+}
